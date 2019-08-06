9d040c6e52ccbccad2eb58d4faffbb259b76e0ed
Jason <jedmeyer@hotmail.com>
2019-08-06 14:40:04 -0500

    Update catfacts.ps1
    got rid of annoying loop

--- catfacts.ps1
+++ catfacts.ps1
@@ -1,11 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-while($true){
-
-if ((Get-Random -Maximum 10000) -lt 1875) {
-   Add-Type -AssemblyName System.Speech
-   $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-   $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'https://catfact.ninja/fact')).fact
-   $speak.Speak("did you know?")
-   $speak.Speak($CatFact)
-}
-}
+Add-Type -AssemblyName System.Speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'https://catfact.ninja/fact')).fact
+$speak.Speak("did you know?")
+$speak.Speak($CatFact)
