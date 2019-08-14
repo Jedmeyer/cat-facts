2e757ce9b7997361cc3aac2b7c553d273dd62a25
Jason <jedmeyer@hotmail.com>
2019-08-14 09:38:47 -0500

    Left old trigger in registration.

--- schedule.ps1
+++ schedule.ps1
@@ -1,2 +1,2 @@
 $span = New-TimeSpan -Hours 2 -Minutes 1
-Register-ScheduledJob -Name "CatFacts" -FilePath "./catfacts.ps1" -Trigger $T -RunEvery $span
+Register-ScheduledJob -Name "CatFacts" -FilePath "./catfacts.ps1" -RunEvery $span
